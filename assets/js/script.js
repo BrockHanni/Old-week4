6dd1ae0da13ab7d5312361e3923e4af6eaec3c19
BrockHanni <hannibr26@gmail.com>
2022-08-11 16:51:27 -0600

    Questions

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+//var that stores all the quiz content
+var questions = [
+{
+    title: "Which brackets are used to make an array?",
+    choices: ["(parentheses)","{curly brackets}","[square brackets]",'"quotes"'],
+    answer:"[square brackets]",
+},
+{
+    title: "In HTML, which sequence do you use to make a comment",
+    choices: ["//","<!-- -->","<--! !-->","{/ /}"],
+    answer:"<!-- -->",
+},
+{
+    title: "In javascript, what command is opposite of .setAttribute?",
+    choices: [".clearAttribute",".deleteAttribute",".resetAttriubute",".removeAttribute"],
+    answer:"",
+},
+{
+    title: "Css can be used to greatly improve the ____ of an HTML form.",
+    choices: ["Layout","Apperance","Color","All of the above"],
+    answer:"All of the above",
+},
+{
+    title: "When was coding invented",
+    choices: ["1883","1917","1956","1993"],
+    answer:"1883",
+},
+]
